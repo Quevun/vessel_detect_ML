5190e0fa26d22b54da2b57ab936cc0b5c30d4058
Quek Yu Yang <quekyuyang@yahoo.com>
2016-09-25 12:32:42 +0900

    testing ridge orientation estimation

--- color/src/test.py
+++ color/src/test.py
@@ -54,8 +54,29 @@ featuremat_maker = featuremat.FeatureMatMaker(img,scales)
 vessel_feature,non_vessel_feature = featuremat_maker.getTrainMat(vessel_ind)
 """
 
-# gradient direction image
+"""# gradient direction image
 img = cv2.imread('../data/IR/quek1.bmp',0)
 sobelx = cv2.Sobel(img,cv2.CV_64F,1,0)
 sobely = cv2.Sobel(img,cv2.CV_64F,0,1)
 grad = np.arctan(sobely/sobelx)
+"""
+
+# ridge orientation
+img = cv2.imread('../data/IR/quek1.bmp',0)
+img = img.astype(np.float64)
+img = cv2.GaussianBlur(img,(15,15),3)
+Lxx = cv2.Sobel(img,cv2.CV_64F,2,0)
+Lyy = cv2.Sobel(img,cv2.CV_64F,0,2)
+Lxy = cv2.Sobel(img,cv2.CV_64F,1,1)
+temp = (Lxx - Lyy)/np.sqrt((Lxx-Lyy)**2 + 4*Lxy**2)
+sin_beta = np.sign(Lxy) * np.sqrt((1-temp)/2)
+cos_beta = np.sqrt((1+temp)/2)
+beta = np.arccos(cos_beta)
+beta2 = np.arcsin(sin_beta)
+beta = np.nan_to_num(np.rad2deg(beta))
+beta2 = np.nan_to_num(np.rad2deg(beta2))
+#beta = (beta/90 * 255).astype(np.uint8)
+beta2 = ((beta2+90)/180 * 255).astype(np.uint8)
+cv2.imshow('stuff',beta2)
+cv2.waitKey()
+cv2.destroyAllWindows()
